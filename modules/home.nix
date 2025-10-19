7b8bde6c1805b897280c744cb3be20371923d740
Noe Thalheim <noe@thalheim.email>
2025-10-20 00:06:53 +0200

    Fix ctrl+r in fzf

--- modules/home.nix
+++ modules/home.nix
@@ -38,10 +38,15 @@
         {
           name = "vi-mode";
           src = pkgs.zsh-vi-mode;
-          # TODO: patch file to use fzf history in insert mode
           file = "share/zsh-vi-mode/zsh-vi-mode.plugin.zsh";
         }
       ];
+      initContent = ''
+        # Re-initialize fzf keybindings after zsh-vi-mode to enable fzf history search (Ctrl+R) in insert mode
+        function zvm_after_init() {
+          zvm_bindkey viins '^R' fzf-history-widget
+        }
+      '';
     };
 
     oh-my-posh = {
