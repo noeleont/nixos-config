4c8e2162c92f31309d9bbb458434d73e6e588819
Noe Thalheim <noe@thalheim.email>
2025-10-19 16:14:32 +0200

    Add yubikey agent support for SSH

--- modules/system/services.nix
+++ modules/system/services.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 {
   # SSH
@@ -24,11 +29,39 @@
   # Caps lock rebinding
   hardware.uinput.enable = lib.mkDefault true;
   services.kanata = {
-    enable = lib.mkDefault  true;
-    keyboards = { main.config = ''(defsrc caps) (deflayer base lctl)'';
+    enable = lib.mkDefault true;
+    keyboards = {
+      main.config = ''(defsrc caps) (deflayer base lctl)'';
     };
   };
 
+  services.yubikey-agent = {
+    enable = true;
+    package = pkgs.yubikey-agent.overrideAttrs (oldAttrs: {
+      patches = (oldAttrs.patches or [ ]) ++ [
+        (pkgs.fetchpatch {
+          name = "pr-155.patch";
+          url = "https://github.com/FiloSottile/yubikey-agent/pull/155.patch";
+          hash = "sha256-tqPydG0NOP/q2VKqv3J6FLHknTYKaJzu6smHeqmdxJA=";
+        })
+      ];
+      vendorHash = "sha256-7yzfLrxt/gttKmt46jeR+5M5E5expt1a7owu86kIRUs=";
+    });
+  };
+
+  systemd.user.services.yubikey-agent = {
+    unitConfig = {
+      After = [
+        "graphical-session.target"
+        "gpg-agent.socket"
+      ];
+    };
+    wantedBy = lib.mkForce [ "graphical-session.target" ];
+  };
+
+  programs.gnupg.agent = {
+    enable = true;
+  };
 
   # Shell
   programs.zsh.enable = lib.mkDefault true;
