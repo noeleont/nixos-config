854350b5b8f75aa294d92f20229abddb5c51075d
Noe Thalheim <noe@thalheim.email>
2025-11-16 12:16:15 +0100

    Fix qemu efi support

--- systems/nixos/configuration.nix
+++ systems/nixos/configuration.nix
@@ -43,17 +43,8 @@
     enable = true;
     qemu = {
       package = pkgs.qemu_kvm;
-      runAsRoot = true;
       swtpm.enable = true;
-      ovmf = {
-        enable = true;
-        packages = [
-          (pkgs.OVMF.override {
-            secureBoot = true;
-            tpmSupport = true;
-          }).fd
-        ];
-      };
+      ovmf.packages = [ pkgs.OVMFFull.fd ];
     };
   };
   virtualisation.docker.enable = true;
