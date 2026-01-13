36d71b2469e8eb9574567a4734a5547823cc0cbd
Noe Thalheim <noe@thalheim.email>
2026-01-13 12:20:03 +0100

    Prepare for remote builds

--- modules/system/nix-settings.nix
+++ modules/system/nix-settings.nix
@@ -5,6 +5,7 @@
     "nix-command"
     "flakes"
   ];
+  nix.settings.trusted-users = [ "@wheel" ];
   nix.extraOptions = ''
     extra-substituters = https://devenv.cachix.org
     extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
