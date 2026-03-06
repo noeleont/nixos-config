a5aec94325331712b365dc22e37d43b617e0a16b
Noe Thalheim <noe@thalheim.email>
2026-03-06 15:25:49 +0100

    add steam disk

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -69,23 +69,12 @@
     virt-manager.enable = true;
   };
 
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  fileSystems."/games/steam" = {
+    device = "/dev/disk/by-uuid/36c6b161-b08d-4950-ad38-aaa765c37e7d";
+    fsType = "ext4";
+    options = [ "noatime" "commit=60" ];
+  };
+
   system.stateVersion = "24.05"; # Did you read the comment?
 
 }
